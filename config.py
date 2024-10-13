8491fb986922021a50dd5c07cf5b9fe990f2b9f9
Sammy930 <178405704+Sammy930@users.noreply.github.com>
2024-10-13 20:52:57 +0100

    Create a configuration file to store constants

---
+++ config.py
@@ -0,0 +1,12 @@
+# Constants
+
+
+K = 1.3806503e-23   #Boltzmann constant (J/K)
+
+Q = 1.60217646e-19  #Electron charge (C)
+
+G_REF = 1000    #Irradiation at standard test conditions (W/m²)
+
+T_REF = 298.15  #Temperature at standard test conditions (K)
+
+VT_REF = 0.025692606103137977   #Thermal voltage at 25°C (V)
