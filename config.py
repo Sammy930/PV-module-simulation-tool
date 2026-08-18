55e1c71aec67c9f4a5b6d9ad955c6fbdb1c4ef39
Sammy930 <178405704+Sammy930@users.noreply.github.com>
2026-08-18 15:52:44 +0100

    Add new constants
    Add the constants Eg_REF and T_2 to be used in
    estimating the reference parameters

--- config.py
+++ config.py
@@ -19,3 +19,8 @@ Q = 1.60217646e-19  #Electron charge (C)
 G_REF = 1000    #Irradiation at standard test conditions (W/m²)
 
 T_REF = 298.15  #Temperature at standard test conditions (K)
+
+T_2 = 348.15  #T_REF offset by 50K to evaluate the open-circuit conditions at a different temperature
+
+Eg_REF = 1.796e-19  #Band gap energy for Si at standard test conditions (J)
+                    #Changes according to semiconductor material
