0c3240d8e4040a6f8c9051f79d3538a16503819a
Sammy930 <178405704+Sammy930@users.noreply.github.com>
2026-08-19 17:05:37 +0100

    Edit comment

--- config.py
+++ config.py
@@ -23,4 +23,4 @@ T_REF = 298.15  #Temperature at standard test conditions (K)
 T_2 = 348.15  #T_REF offset by 50K to evaluate the open-circuit conditions at a different temperature
 
 Eg_REF = 1.796e-19  #Band gap energy for Si at standard test conditions (J)
-                    #Changes according to semiconductor material
+                    #Modify according to semiconductor material
