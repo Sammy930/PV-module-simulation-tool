cc30b61b6a294618e7b63f4932039525b0c45730
Sammy930 <178405704+Sammy930@users.noreply.github.com>
2026-09-04 17:08:19 +0100

    Display Pmax_ref in results

--- simulation_script.py
+++ simulation_script.py
@@ -28,103 +28,103 @@ _ = fr_i18n.gettext
 Ta, G = 25, 1000
 
 with file.open("a", encoding="utf-8") as file:
-    for Datasheet in Data:  
+    for Datasheet in Data:
 
-        #Module specifications  
+        #Module specifications
 
-        Isc_ref, Voc_ref, Imp_ref, Vmp_ref, Ki, Kv, NOCT = map(float, islice(Datasheet.values(), 1, 8))         
+        Isc_ref, Voc_ref, Imp_ref, Vmp_ref, Ki, Kv, NOCT = map(float, islice(Datasheet.values(), 1, 8))
 
-        length, width = Datasheet["length"], Datasheet["width"]     
+        length, width = Datasheet["length"], Datasheet["width"]
 
-        model_ref = Datasheet["model_ref"]      
+        model_ref = Datasheet["model_ref"]
 
-        Pmax_ref = Imp_ref*Vmp_ref      
+        Pmax_ref = Imp_ref*Vmp_ref
 
-        Ki, Kv = (Ki*Isc_ref)/100, (Kv*Voc_ref)/100       
+        Ki, Kv = (Ki*Isc_ref)/100, (Kv*Voc_ref)/100
 
         match TEMP_SETTING:
-            case 'Ambient': 
+            case 'Ambient':
                 T = (Ta - 32)*(5/9) if UNITS['temperature'] == 'F' else Ta
                 T = (NOCT - 20)*G/800 + T + 273.15
                 Ta = T - 273.15 if UNITS["temperature"] == 'C' else (T - 273.15)*(9/5) + 32
             case 'Cell':
-                T = (Ta - 32)*(5/9) + 273.15 if UNITS['temperature'] == 'F' else Ta + 273.15        
+                T = (Ta - 32)*(5/9) + 273.15 if UNITS['temperature'] == 'F' else Ta + 273.15  
 
-        #Extract reference params       
+        #Extract reference params
 
         def equations(vars):
-            """Uses datasheet values to define the five equations of the De Soto model 
-            and their respective variables."""  
+            """Uses datasheet values to define the five equations of the De Soto model
+            and their respective variables."""
 
-            Iph_ref, Io_ref, a_ref, Rs_ref, Rsh_ref = vars      
+            Iph_ref, Io_ref, a_ref, Rs_ref, Rsh_ref = vars
 
-            E = np.exp((Vmp_ref + Imp_ref*Rs_ref)/a_ref)    #Extracted expression for readability       
+            E = np.exp((Vmp_ref + Imp_ref*Rs_ref)/a_ref)    #Extracted expression for readability
 
             #Open circuit conditions evaluated at T_2
             Eg_2 = Eg_REF*(1 - 0.0002677*(T_2 - T_REF))
             Iph_2 = Iph_ref + Ki*(T_2 - T_REF)
             Io_2 = Io_ref*((T_2/T_REF)**3)*np.exp((Eg_REF/(K*T_REF)) - (Eg_2/(K*T_2)))
             Voc_2 = Voc_ref + Kv*(T_2 - T_REF)
-            a_2 = a_ref*(T_2/T_REF)     
+            a_2 = a_ref*(T_2/T_REF)
 
             #De Soto equations
             eq1 = (Iph_ref - Io_ref*(np.exp((Isc_ref*Rs_ref)/a_ref) - 1) - Isc_ref*(1 + (Rs_ref/Rsh_ref)))/Isc_ref
             eq2 = (Iph_ref - Io_ref*(np.exp(Voc_ref/a_ref) - 1) - Voc_ref/Rsh_ref)/Voc_ref
             eq3 = (Iph_ref - Io_ref*(E - 1) - (Vmp_ref + Imp_ref*(Rs_ref + Rsh_ref))/Rsh_ref)/Imp_ref
             eq4 = (Imp_ref/Vmp_ref - ((Io_ref/a_ref*E + 1/Rsh_ref)/(1 + (Io_ref*Rs_ref/a_ref)*E + Rs_ref/Rsh_ref)))*(Vmp_ref/Imp_ref)
-            eq5 = (Iph_2 - Io_2*(np.exp(Voc_2/a_2) - 1) - Voc_2/Rsh_ref)/Isc_ref    
+            eq5 = (Iph_2 - Io_2*(np.exp(Voc_2/a_2) - 1) - Voc_2/Rsh_ref)/Isc_ref
 
-            return [eq1, eq2, eq3, eq4, eq5]        
+            return [eq1, eq2, eq3, eq4, eq5]
 
         a_guess = (Vmp_ref - Voc_ref)/(np.log(1 - Imp_ref/Isc_ref))
-        initial_guesses = [Isc_ref, Isc_ref*(np.exp(-Voc_ref/a_guess)), a_guess, 0, np.inf]         
+        initial_guesses = [Isc_ref, Isc_ref*(np.exp(-Voc_ref/a_guess)), a_guess, 0, np.inf]
 
-        X = fsolve(equations, initial_guesses)      
+        X = fsolve(equations, initial_guesses)
 
 
-        #Params translated to (T,G)     
+        #Params translated to (T,G)
 
         Iph = (G/G_REF)*(X[0] + Ki*(T - T_REF))
         Eg = Eg_REF*(1 - 0.0002677*(T - T_REF))
         Io = X[1]*((T/T_REF)**3)*np.exp((Eg_REF/(K*T_REF)) - (Eg/(K*T)))
         a = X[2]*(T/T_REF)
         Rs = X[3]
-        Rsh = X[4]*(G_REF/G)        
+        Rsh = X[4]*(G_REF/G)
 
 
         IV = generate_iv(Isc_ref, Voc_ref, Iph, Io, Kv, a, T, Rs, Rsh)
         voltage = IV[0]
         current = IV[1]
-        power = np.multiply(current, voltage)       
+        power = np.multiply(current, voltage)
 
 
-        #Simulation results     
+        #Simulation results
 
         Pmax = max(power)
         Vmp = voltage[list(power).index(Pmax)]
         Imp = current[list(power).index(Pmax)]
         fill_factor = Pmax/(current[0]*voltage[-1])
-        e = abs(Pmax - Pmax_ref)/Pmax_ref*100       
+        e = abs(Pmax - Pmax_ref)/Pmax_ref*100
 
         file.write(
             "\n" + _(f"-----------------------------------RESULTS FOR {model_ref}-----------------------------------") + "\n" +
-            _("The maximum power yielded by the module is: {0} Watt").format(ceil((Pmax*100))/100) + "\n" + 
+            _("The maximum power yielded by the module is: {0} Watt (Pmax_ref = {1})").format(ceil((Pmax*100))/100, ceil(Pmax_ref*100)/100) + "\n" +
             _("The max power point is estimated at I = {0} Amps and  V = {1} Volts").format(ceil((Imp*100))/100, ceil((Vmp*100))/100) + "\n" +
-            _("Fill Factor = {0}").format(ceil((fill_factor*100))/100) + "\n")   
+            _("Fill Factor = {0}").format(ceil((fill_factor*100))/100) + "\n")
 
         if is_float_regex(length) and is_float_regex(width):
             match UNITS['length']:
                 case 'mm':
                     A = float(length)*float(width)*1e-6
                 case 'in':
-                    A = (float(length)*float(width))/1550        
-            efficiency = (Pmax/(G*A))*100     
-            file.write(_("Efficiency = {0} %").format(ceil((efficiency*100))/100) + "\n")       
+                    A = (float(length)*float(width))/1550
+            efficiency = (Pmax/(G*A))*100
+            file.write(_("Efficiency = {0} %").format(ceil((efficiency*100))/100) + "\n")
 
-        file.write(f"Accuracy = {ceil(e*100)/100} %" + "\n")    
+        file.write(f"Accuracy = {ceil(e*100)/100} %" + "\n")
 
 
-        #Results visualization      
+        #Results visualization
 
         fig = plt.figure(num="IV/PV Plot")
         fig.suptitle(_("CHARACTERISTIC CURVES FOR {}").format(model_ref), fontname=FONT['family'], weight=FONT['weight'], color=FONT['color'], size="18")
@@ -134,16 +134,16 @@ with file.open("a", encoding="utf-8") as file:
             color=FONT['color'], fontsize=10,
         )
         fig.set_size_inches(9, 9)
-        fig.patch.set_facecolor("#ffffff")      
+        fig.patch.set_facecolor("#ffffff")
 
         IV = plt.subplot(211)
         PV = plt.subplot(212, sharex=IV)
-        plt.subplots_adjust(hspace=0)       
+        plt.subplots_adjust(hspace=0)
 
         IV.spines["left"].set_color("none")
         IV.spines["right"].set_color("none")
         IV.spines["top"].set_color("none")
-        IV.tick_params(axis='y', colors=FONT['color'])      
+        IV.tick_params(axis='y', colors=FONT['color'])
 
         IV.plot(voltage, current, "#7a76c2", linewidth=2, label="I = f(V)")
         IV.set_xlim(0)
@@ -152,7 +152,7 @@ with file.open("a", encoding="utf-8") as file:
         IV.set_ylabel(_("Current") + "\n" + _("(Amp)"), fontdict=FONT, rotation=0, loc="center", labelpad=32)
         IV.legend(loc="upper right")
         IV.grid(c='#ffffff')
-        IV.set_facecolor('#eaeaf2')     
+        IV.set_facecolor('#eaeaf2')
 
         PV.spines["left"].set_color("none")
         PV.spines["right"].set_color("none")
@@ -160,7 +160,7 @@ with file.open("a", encoding="utf-8") as file:
         PV.spines['bottom'].set_color(FONT['color'])
         PV.tick_params(axis='x', colors=FONT['color'])
         PV.tick_params(axis='y', colors=FONT['color'])
-        PV.xaxis.label.set_color(FONT['color'])     
+        PV.xaxis.label.set_color(FONT['color'])
 
         PV.plot(voltage, power, "#f62196", linewidth=2, label="P = f(V)")
         PV.plot(Vmp, Pmax, "#f6a0be", marker="o", label=_("Max power point"))    #highlight max power point
@@ -169,7 +169,7 @@ with file.open("a", encoding="utf-8") as file:
         PV.set_ylabel(_("Power") + "\n(Watt)", fontdict=FONT, rotation=0, loc="center", labelpad=32)
         PV.legend(loc="upper left")
         PV.grid(c='#ffffff')
-        PV.set_facecolor('#eaeaf2')     
+        PV.set_facecolor('#eaeaf2')
 
         plt.savefig(f"test_results/{model_ref}.png", dpi=300, bbox_inches="tight")
         plt.close(fig)
