8264d45bfce6419235f2e44b2e97d2333cb7ba53
Sammy930 <178405704+Sammy930@users.noreply.github.com>
2026-09-18 22:04:52 +0100

    Change initial guesses list to np.array type

--- simulation_script.py
+++ simulation_script.py
@@ -81,7 +81,7 @@ def equations(vars):
     return [eq1, eq2, eq3, eq4, eq5]
 
 a_guess = (Vmp_ref - Voc_ref)/(np.log(1 - Imp_ref/Isc_ref))
-initial_guesses = [Isc_ref, Isc_ref*(np.exp(-Voc_ref/a_guess)), a_guess, 0, np.inf] 
+initial_guesses = np.array([Isc_ref, Isc_ref*(np.exp(-Voc_ref/a_guess)), a_guess, 0, np.inf])
 
 X = fsolve(equations, initial_guesses)
 
